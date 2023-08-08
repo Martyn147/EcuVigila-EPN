f7ec45abbce607c4f5dcd1716fa62ecd21812209
Martyn147 <mrosero.sfq@gmail.com>
2023-08-08 06:56:58 -0500

    Creacion de eventos

--- app/src/main/java/com/example/ecuvigila/Mapa.java
+++ app/src/main/java/com/example/ecuvigila/Mapa.java
@@ -1,64 +1,66 @@
 package com.example.ecuvigila;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Mapa#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Mapa extends Fragment {
+public class Mapa extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Mapa() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Mapa.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Mapa newInstance(String param1, String param2) {
-        Mapa fragment = new Mapa();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false);
+        View view = inflater.inflate(R.layout.fragment_mapa, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
+        return view;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            mMap.setMyLocationEnabled(true);
+
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(requireActivity(), location -> {
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    LatLng currentLocation = new LatLng(latitude, longitude);
+
+                    mMap.addMarker(new MarkerOptions().position(currentLocation).title("My Location"));
+
+                    // Move camera to current location with zoom level
+                    float zoomLevel = 15f; // You can adjust the zoom level as needed
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, zoomLevel));
+                }
+            });
+        }
+    }
+
 }
--- app/src/main/java/com/example/ecuvigila/ReportarEvento.java
+++ app/src/main/java/com/example/ecuvigila/ReportarEvento.java
@@ -1,64 +1,98 @@
 package com.example.ecuvigila;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReportarEvento#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ReportarEvento extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ReportarEvento extends Fragment implements OnMapReadyCallback {
 
-    public ReportarEvento() {
-        // Required empty public constructor
-    }
+    private GoogleMap mMap;
+    private Spinner spinnerNivelPeligro;
+    private Spinner spinnerDetalles;
+    private EditText editTextComentario;
+    private Button btnGuardar;
+    private DatabaseReference databaseReference;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReportarEvento.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReportarEvento newInstance(String param1, String param2) {
-        ReportarEvento fragment = new ReportarEvento();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_reportar_evento, container, false);
+
+        spinnerNivelPeligro = view.findViewById(R.id.spinner_nivel_peligro);
+        spinnerDetalles = view.findViewById(R.id.spinner_detalles);
+        editTextComentario = view.findViewById(R.id.edit_text_comentario);
+        btnGuardar = view.findViewById(R.id.btn_guardar);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("eventos");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        ArrayAdapter<CharSequence> nivelPeligroAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.nivel_peligro, android.R.layout.simple_spinner_item);
+        nivelPeligroAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerNivelPeligro.setAdapter(nivelPeligroAdapter);
+
+        ArrayAdapter<CharSequence> detallesAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.detalles_evento, android.R.layout.simple_spinner_item);
+        detallesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDetalles.setAdapter(detallesAdapter);
+
+        btnGuardar.setOnClickListener(v -> guardarEvento());
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Agrega el marcador en una ubicación por defecto
+        LatLng ubicacionPorDefecto = new LatLng(-0.180525, -78.467834);
+        mMap.addMarker(new MarkerOptions().position(ubicacionPorDefecto).title("Marcador por defecto"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacionPorDefecto, 15f));
+
+        // Configurar el evento de clic en el mapa para agregar un marcador
+        mMap.setOnMapClickListener(latLng -> {
+            mMap.clear(); // Limpia los marcadores existentes
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Nuevo marcador"));
+        });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reportar_evento, container, false);
+    private void guardarEvento() {
+        String nivelPeligro = spinnerNivelPeligro.getSelectedItem().toString();
+        String detallesEvento = spinnerDetalles.getSelectedItem().toString();
+        String comentario = editTextComentario.getText().toString();
+        LatLng ubicacionMarcador = mMap.getCameraPosition().target; // Obtener la ubicación del marcador
+
+        DatabaseReference nuevoEventoReference = databaseReference.push();
+        nuevoEventoReference.child("nivel_peligro").setValue(nivelPeligro);
+        nuevoEventoReference.child("detalles_evento").setValue(detallesEvento);
+        nuevoEventoReference.child("comentario").setValue(comentario);
+        nuevoEventoReference.child("latitud").setValue(ubicacionMarcador.latitude);
+        nuevoEventoReference.child("longitud").setValue(ubicacionMarcador.longitude);
+
+        // Limpiar los campos después de guardar
+        spinnerNivelPeligro.setSelection(0);
+        spinnerDetalles.setSelection(0);
+        editTextComentario.setText("");
+        mMap.clear(); // Limpia el marcador del mapa
     }
 }
