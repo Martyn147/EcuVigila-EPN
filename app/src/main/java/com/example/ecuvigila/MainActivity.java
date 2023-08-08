ab439eec53dfdf9c6ac04d965ee6028c8ab76a45
Jimy Bryan Calvo Mullo <74626077+JimyCalvo@users.noreply.github.com>
2023-08-08 00:49:57 -0500

    cambios

--- app/src/main/java/com/example/ecuvigila/MainActivity.java
+++ app/src/main/java/com/example/ecuvigila/MainActivity.java
@@ -1,74 +1,87 @@
 package com.example.ecuvigila;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Obtener referencias a los botones del menú
-        Button btnMapa = findViewById(R.id.btnMapa);
-        Button btnInfoEmergencia = findViewById(R.id.btnInfoEmergencia);
-        Button btnCrearAviso = findViewById(R.id.btnCrearAviso);
-        Button btnNumerosEmergencia = findViewById(R.id.btnNumerosEmergencia);
-        Button btnAjustes = findViewById(R.id.btnAjustes);
-        Button btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        Toolbar toolbar = findViewById(R.id.toolbar1); //Ignore red line errors
+        setSupportActionBar(toolbar);
 
-        // Implementar eventos onClick para cada botón del menú
-       btnMapa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Abrir la actividad "MapActivity"
-                startActivity(new Intent(MainActivity.this, MapsActivity.class));
-            }
-        });
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
 
-        btnInfoEmergencia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Abrir la actividad "Informacion_de_emergencia"
-                startActivity(new Intent(MainActivity.this, Informacion_de_emergencia.class));
-            }
-        });
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
+                break;
 
-        btnCrearAviso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Abrir la actividad "CrearAvisoActivity"
-                startActivity(new Intent(MainActivity.this, CrearAvisoActivity.class));
-            }
-        });
+            case R.id.nav_map:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Mapa()).commit();
+                break;
 
-        btnNumerosEmergencia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Abrir la actividad "NumerosEmergenciaActivity"
-                startActivity(new Intent(MainActivity.this, NumerosEmergenciaActivity.class));
-            }
-        });
+            case R.id.nav_infEmergencia:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InfoEmergencia()).commit();
+                break;
 
-        btnAjustes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Abrir la actividad "AjustesActivity"
-                startActivity(new Intent(MainActivity.this, AjustesActivity.class));
-            }
-        });
+            case R.id.nav_numeroEmergencia:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConEmergencia()).commit();
+                break;
 
-        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Implementar aquí la lógica para cerrar sesión si es necesario
-            }
-        });
+            case R.id.nav_Reportar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReportarEvento()).commit();
+                break;
+            case R.id.btnAjustes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Ajustes()).commit();
+                break;
+
+            case R.id.nav_logout:
+                Toast.makeText(this, "Cerrar Sesión!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
