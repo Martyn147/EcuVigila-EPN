263f92a668cfe399f0dd4c43266813c2444f94a1
Martyn147 <mrosero.sfq@gmail.com>
2023-09-05 05:57:40 -0500

    Nuevo proyecto corredores

--- app/src/main/java/com/example/ecuvigila/Mapa.java
+++ app/src/main/java/com/example/ecuvigila/Mapa.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +15,9 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,13 +30,27 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Mapa extends Fragment implements OnMapReadyCallback, GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private DatabaseReference eventosRef;
+    private DatabaseReference ubicacionUsuarioRef;
+    private String usuarioActualId;
+    private Map<Marker, String> markerEventoMap = new HashMap<>();
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private LocationCallback locationCallback;
+    private ValueEventListener ubicacionesUsuariosListener;
+    private Map<String, Marker> usuariosMarkers = new HashMap<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,6 +63,9 @@ public class Mapa extends Fragment implements OnMapReadyCallback, GoogleMap.Info
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
 
         eventosRef = FirebaseDatabase.getInstance().getReference("eventos");
+        ubicacionUsuarioRef = FirebaseDatabase.getInstance().getReference("ubicaciones_usuarios");
+
+        usuarioActualId = obtenerIdUsuarioActual(); // Implementa este método para obtener el ID del usuario actual
 
         return view;
     }
@@ -54,6 +73,50 @@ public class Mapa extends Fragment implements OnMapReadyCallback, GoogleMap.Info
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        // Agrega un ValueEventListener para escuchar las ubicaciones de los usuarios en Firebase
+        ubicacionesUsuariosListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Itera a través de los datos y actualiza o agrega los marcadores de usuarios en el mapa
+                for (DataSnapshot usuarioSnapshot : dataSnapshot.getChildren()) {
+                    String usuarioId = usuarioSnapshot.getKey();
+                    Double latitud = usuarioSnapshot.child("latitud").getValue(Double.class);
+                    Double longitud = usuarioSnapshot.child("longitud").getValue(Double.class);
+                    String correo = usuarioSnapshot.child("correo").getValue(String.class);
+
+                    if (usuarioId != null && latitud != null && longitud != null) {
+                        // Verifica si el marcador ya existe
+                        if (usuariosMarkers.containsKey(usuarioId)) {
+                            // Si existe, actualiza su posición
+                            Marker marker = usuariosMarkers.get(usuarioId);
+                            LatLng nuevaUbicacion = new LatLng(latitud, longitud);
+                            marker.setPosition(nuevaUbicacion);
+                        } else {
+                            // Si no existe, crea un nuevo marcador
+                            LatLng ubicacionUsuario = new LatLng(latitud, longitud);
+                            Marker marker = mMap.addMarker(new MarkerOptions().position(ubicacionUsuario).title("Usuario " + usuarioId));
+
+                            // Asocia el correo del usuario con el marcador
+                            marker.setTag(correo);
+
+                            usuariosMarkers.put(usuarioId, marker);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Manejar errores de Firebase
+            }
+        };
+
+
+
+        // Agrega el ValueEventListener a la ubicación de los usuarios en Firebase
+        ubicacionUsuarioRef.addValueEventListener(ubicacionesUsuariosListener);
+
 
         if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -66,13 +129,33 @@ public class Mapa extends Fragment implements OnMapReadyCallback, GoogleMap.Info
                     double longitude = location.getLongitude();
                     LatLng currentLocation = new LatLng(latitude, longitude);
 
-                    mMap.addMarker(new MarkerOptions().position(currentLocation).title("Mi Ubicación"));
-
-                    float zoomLevel = 15f;
+                    float zoomLevel = 19f;
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, zoomLevel));
+
+                    // Iniciar la obtención de ubicación del usuario
+                    guardarUbicacionUsuario(latitude, longitude);
                 }
             });
         }
+
+        // Configurar un listener de ubicación para seguir la ubicación en tiempo real
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location newLocation = locationResult.getLastLocation();
+                    LatLng newLatLng = new LatLng(newLocation.getLatitude(), newLocation.getLongitude());
+
+                    // Actualiza la ubicación del usuario en Firebase
+                    guardarUbicacionUsuario(newLatLng.latitude, newLatLng.longitude);
+                }
+            }
+        };
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000); // Intervalo en milisegundos para obtener actualizaciones de ubicación
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+
 
         mMap.setInfoWindowAdapter(this);
         mMap.setOnInfoWindowClickListener(this);
@@ -88,16 +171,14 @@ public class Mapa extends Fragment implements OnMapReadyCallback, GoogleMap.Info
                     if (latitud != null && longitud != null) {
                         LatLng eventLocation = new LatLng(latitud, longitud);
                         Marker marker = mMap.addMarker(new MarkerOptions().position(eventLocation).title(detallesEvento));
-                        marker.setTag(eventoSnapshot.getKey()); // Guardar el ID del evento como etiqueta
-
-                        Log.d("Mapa", "Marcador agregado con título: " + detallesEvento);
+                        markerEventoMap.put(marker, eventoSnapshot.getKey());
                     }
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error al obtener datos: " + databaseError.getMessage());
+                // Manejar errores de Firebase
             }
         });
     }
@@ -107,47 +188,82 @@ public class Mapa extends Fragment implements OnMapReadyCallback, GoogleMap.Info
         return null; // No usaremos ventanas personalizadas, solo el contenido predeterminado
     }
 
-
     @Override
     public View getInfoContents(Marker marker) {
         View view = getLayoutInflater().inflate(R.layout.info_window_layout, null);
 
-        String eventoId = (String) marker.getTag();
-        if (eventoId != null) {
-            DatabaseReference eventoRef = eventosRef.child(eventoId);
-            eventoRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    TextView tvNivelPeligro = view.findViewById(R.id.tvNivelPeligro);
-                    TextView tvDetallesEvento = view.findViewById(R.id.tvDetallesEvento);
-                    TextView tvComentario = view.findViewById(R.id.tvComentario);
+        // Obtén el correo asociado al marcador
+        String correo = (String) marker.getTag();
 
-                    String nivelPeligro = dataSnapshot.child("nivel_peligro").getValue(String.class);
-                    String detallesEvento = dataSnapshot.child("detalles_evento").getValue(String.class);
-                    String comentario = dataSnapshot.child("comentario").getValue(String.class);
-
-                    tvNivelPeligro.setText("Nivel de peligro: " + nivelPeligro);
-                    tvDetallesEvento.setText("Detalles del evento: " + detallesEvento);
-                    tvComentario.setText("Comentario: " + comentario);
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Log.e("Firebase", "Error al obtener detalles del evento: " + databaseError.getMessage());
-                }
-            });
+        if (correo != null) {
+            TextView tvCorreo = view.findViewById(R.id.tvCorreo);
+            tvCorreo.setText("Correo: " + correo);
         }
 
         return view;
     }
 
 
-
-
-
-
     @Override
     public void onInfoWindowClick(Marker marker) {
         // Aquí puedes implementar alguna acción al hacer clic en la ventana de información del marcador
     }
+
+    // Agrega la función para obtener el ID del usuario actual según tu sistema de autenticación
+    private String obtenerIdUsuarioActual() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            return user.getUid();
+        } else {
+            // Si el usuario no está autenticado, devuelve null o realiza alguna otra acción
+            return null;
+        }
+    }
+    private void obtenerUbicacionActual() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Si los permisos no están otorgados, solicitarlos al usuario
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, MY_PERMISSIONS_REQUEST_LOCATION);
+        } else {
+            // Si los permisos ya están otorgados, puedes iniciar la obtención de ubicación
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(requireActivity(), location -> {
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    LatLng currentLocation = new LatLng(latitude, longitude);
+
+                    float zoomLevel = 19f;
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, zoomLevel));
+
+                    // Resto del código para configurar la ubicación en tiempo real
+                }
+            });
+        }
+    }
+    private void guardarUbicacionUsuario(double latitud, double longitud) {
+        if (usuarioActualId != null) {
+            // Obtener el correo del usuario actualmente autenticado
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            FirebaseUser usuarioActual = auth.getCurrentUser();
+
+            if (usuarioActual != null) {
+                String correoUsuario = usuarioActual.getEmail();
+
+                // Crear un mapa con los datos de ubicación, incluyendo el correo
+                Map<String, Object> ubicacion = new HashMap<>();
+                ubicacion.put("latitud", latitud);
+                ubicacion.put("longitud", longitud);
+                ubicacion.put("timestamp", ServerValue.TIMESTAMP);
+                ubicacion.put("correo", correoUsuario);
+
+                // Guardar los datos de ubicación en Firebase Realtime Database
+                ubicacionUsuarioRef.child(usuarioActualId).setValue(ubicacion);
+            }
+        }
+    }
+
 }
