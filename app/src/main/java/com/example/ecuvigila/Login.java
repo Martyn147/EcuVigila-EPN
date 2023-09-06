0403bf6bbcefbe6d87a384697a32f84d86e6363d
Martyn147 <mrosero.sfq@gmail.com>
2023-09-06 04:08:52 -0500

    b

--- app/src/main/java/com/example/ecuvigila/Login.java
+++ app/src/main/java/com/example/ecuvigila/Login.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,19 +16,21 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 import android.Manifest;
-import android.content.pm.PackageManager;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+
+
 
 public class Login extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
 
     private FirebaseAuth mAuth;
+
     EditText editEmail, editPass;
     Button btnRegistrar, btnIngresar;
 
@@ -73,7 +76,7 @@ public class Login extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         editEmail = findViewById(R.id.editEmail);
         editPass = findViewById(R.id.editPass);
-        btnRegistrar = findViewById(R.id.btnRegistrar);
+//        btnRegistrar = findViewById(R.id.btnRegistrar);
         btnIngresar = findViewById(R.id.btnIngresar);
 
         btnIngresar.setOnClickListener(new View.OnClickListener() {
@@ -90,69 +93,51 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        btnRegistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = editEmail.getText().toString().trim();
-                String pass = editPass.getText().toString().trim();
-
-                if(email.isEmpty() || pass.isEmpty()){
-                    Toast.makeText(Login.this, "Ingresar los datos", Toast.LENGTH_SHORT).show();
-                } else {
-                    registerUser(email, pass);
-                }
-            }
-        });
-    }
-
-    private void registerUser(String email, String pass) {
-        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    finish();
-                    startActivity(new Intent(Login.this, MainActivity.class));
-                    Toast.makeText(Login.this, "Cuentra creada!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(Login.this, "Error al crear cuenta...", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String email = editEmail.getText().toString().trim();
+//                String pass = editPass.getText().toString().trim();
+//
+//                if(email.isEmpty() || pass.isEmpty()){
+//                    Toast.makeText(Login.this, "Ingresar los datos", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    registerUser(email, pass);
+//                }
+//            }
+//        });
     }
 
     private void loginUser(String email, String pass) {
-        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    finish();
+            public void onSuccess(AuthResult authResult) {
+                finish();
 
-                    if(email.equals("ppc@gmail.com")){
-                        startActivity(new Intent(Login.this, GestionUsuarios.class));
-                    } else {
-                        startActivity(new Intent(Login.this, MainActivity.class));
-                    }
-
-                    Toast.makeText(Login.this, "Bienvenido!", Toast.LENGTH_SHORT).show();
+                if(email.equals("ppc@gmail.com")){
+                    startActivity(new Intent(Login.this, GestionUsuarios.class));
+                } else {
+                    startActivity(new Intent(Login.this, MainActivity.class));
                 }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(Login.this, "Error al iniciar sesión...", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(Login.this, "Bienvenido!", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser user = mAuth.getCurrentUser();
         if(user != null){
-            startActivity(new Intent(Login.this, MainActivity.class));
+            String email = user.getEmail();
+
+            if(email.equals("ppc@gmail.com")){
+                startActivity(new Intent(Login.this, GestionUsuarios.class));
+            } else {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
             finish();
         }
     }
